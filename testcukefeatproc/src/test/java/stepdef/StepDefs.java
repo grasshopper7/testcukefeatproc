cca0e141b3695027ee3eb96c251cef4707f8e144
grasshopper7 <grass.hopper.moc@gmail.com>
2019-07-06 22:17:28 +0530

    Updated dependencies

--- testcukefeatproc/src/test/java/stepdef/StepDefs.java
+++ testcukefeatproc/src/test/java/stepdef/StepDefs.java
@@ -7,12 +7,15 @@ public class StepDefs {
 	
 	@Given("Process given datatable")
 	public void datatable(DataTable data) {
-		System.out.println(data);		
+		//System.out.println("Thread ID " + Thread.currentThread().getId());
+		System.out.println(data);
+		System.out.println("");
 	}
 	
 	@Given("Do {string} when {string} and {string}")
 	public void scenariooutline(String one, String two, String three) {
-		System.out.println(String.format("Do %s when %s and %s", one, two, three));
+		//System.out.println("Thread ID " + Thread.currentThread().getId());
+		System.out.format("Do %s when %s and %s", one, two, three);
 		System.out.println("");
 	}
 }
